dcf5b840f85cc0a4f9abd216791054138b1647ba
shemil076 <shemil076@gmail.com>
2023-12-18 23:26:06 +0530

    Implemented the background and partialy updated the weatherNowView :dizzy:

---
+++ CWK2Template/Views/Background.swift
@@ -0,0 +1,77 @@
+//
+//  Background.swift
+//  CWK2Template
+//
+//  Created by Pramuditha Karunarathna on 2023-12-18.
+//
+
+import SwiftUI
+
+struct Background: View {
+    
+    @State private var blurRadius: CGFloat = 0
+    @State private var scale: CGFloat = 1.0
+    
+    var body: some View {
+        ZStack{
+            
+            VStack{
+                Image("Top-Glow")
+                Spacer()
+            }.ignoresSafeArea()
+            VStack{
+                HStack {
+                    Spacer()
+                    Image("Top-right")
+                        .padding(.top)
+                        .blur(radius: blurRadius)
+                        .onAppear {
+                            withAnimation(Animation.easeInOut(duration: 3.0).repeatForever(autoreverses: true)) {
+                                blurRadius = 5
+                            }
+                        }
+                }
+                Spacer()
+                
+            }.ignoresSafeArea()
+            
+            Spacer()
+            Image("Main")
+                .scaleEffect(scale)
+                .onAppear {
+                    withAnimation(Animation.easeInOut(duration: 10.0).repeatForever(autoreverses: true)) {
+                        scale = 1.5
+                    }
+                }
+            
+            VStack{
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("Bottom-right")
+                        .padding(.top)
+                        .blur(radius: blurRadius)
+                        .onAppear {
+                            withAnimation(Animation.easeInOut(duration: 3.0).repeatForever(autoreverses: true)) {
+                                blurRadius = 5
+                            }
+                        }
+                    
+                }
+                
+            }.ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("Bottom-Glow")
+                
+            }.ignoresSafeArea()
+            
+        }
+
+    }
+}
+
+#Preview {
+    Background()
+}
--- CWK2Template/Views/WeatherNowView.swift
+++ CWK2Template/Views/WeatherNowView.swift
@@ -12,51 +12,129 @@ struct WeatherNowView: View {
     @State private var isLoading = false
     @State private var temporaryCity = ""
     var body: some View {
+        ZStack{
+            
+            
+            Background()
+            
+            
+            VStack{
+                Spacer()
+                RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.gray, lineWidth: 1)
+                            .frame(height: 40)
+                            .overlay(
+                                HStack {
 
-        VStack{
-            HStack{
-                Text("Change Location")
+                                    TextField("Enter text", text: $temporaryCity)
+                                        .padding(.horizontal, 10)
+                                        .foregroundColor(.white)
 
-                TextField("Enter New Location", text: $temporaryCity)
-                    .onSubmit {
+                                    Image(systemName: "magnifyingglass")
+                                        .foregroundColor(.gray)
+                                        .padding(.trailing, 10)
 
-                        weatherMapViewModel.city = temporaryCity
-                        Task {
-                            do {
-                                // write code to process user change of location
-                                
-                                try await weatherMapViewModel.getCoordinatesForCity(cityName: "\(temporaryCity)")
-                                
-//                                print("NEW LOCATION")
-//                                print("\(newLocation)")
-//                                print("\(weatherMapViewModel.coordinates?.latitude)")
-//                                print("\(weatherMapViewModel.coordinates?.longitude)")
-                                
-                                _ = try await weatherMapViewModel.loadData(lat: weatherMapViewModel.coordinates?.latitude ?? Constants.defualtLatitude, lon: weatherMapViewModel.coordinates?.longitude ?? Constants.defualtLongitude)
-                                
-//                                print("========================================><===========")
-                                
-                                
-                                
-                            } catch {
-                                print("Error: \(error)")
-                                isLoading = false
-                            }
+
+                                }
+                                    .onSubmit {
+
+                                        weatherMapViewModel.city = temporaryCity
+                                        Task {
+                                            do {
+                                                // write code to process user change of location
+                                                
+                                                try await weatherMapViewModel.getCoordinatesForCity(cityName: "\(temporaryCity)")
+                                                
+                //                                print("NEW LOCATION")
+                //                                print("\(newLocation)")
+                //                                print("\(weatherMapViewModel.coordinates?.latitude)")
+                //                                print("\(weatherMapViewModel.coordinates?.longitude)")
+                                                
+                                                _ = try await weatherMapViewModel.loadData(lat: weatherMapViewModel.coordinates?.latitude ?? Constants.defualtLatitude, lon: weatherMapViewModel.coordinates?.longitude ?? Constants.defualtLongitude)
+                                                
+                //                                print("========================================><===========")
+                                                
+                                                
+                                                
+                                            } catch {
+                                                print("Error: \(error)")
+                                                isLoading = false
+                                            }
+                                        }
+                                    }
+                            )
+                            .padding()
+
+                Spacer()
+
+                Text("\(weatherMapViewModel.city)")
+                    .font(.custom("", size: 40))
+                    .foregroundColor(.white)
+                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(Blur(style: .systemMaterial).opacity(0.3))
+                                    .cornerRadius(10)
+                                    .padding()
+
+                Spacer()
+
+                ZStack{
+                    Image("temp")
+                        .resizable()
+                        .scaledToFit()
+
+                    HStack{
+                        Spacer()
+                        
+                        if let forecast = weatherMapViewModel.weatherDataModel {
+                            Text("\((Double)(forecast.current.temp), specifier: "%.2f") ºC")
+                                .bold()
+                                .font(.custom("Poppins", size: 60))
+                                .foregroundStyle(.white)
+                                .padding(.trailing,10)
                         }
+                        
                     }
-            }
-            .bold()
-            .font(.system(size: 20))
-            .padding(10)
-            .shadow(color: .blue, radius: 10)
-            .cornerRadius(10)
-            .fixedSize()
-            .font(.custom("Arial", size: 26))
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .cornerRadius(15)
+
+
+                }.padding(.bottom, 10)
+
+            }.padding()
+            
+            
+            
             VStack{
                 HStack{
-                    Text("Current Location: \(weatherMapViewModel.city)")
+//                    Text("Change Location")
+
+//                    TextField("Enter New Location", text: $temporaryCity)
+//                        .onSubmit {
+//
+//                            weatherMapViewModel.city = temporaryCity
+//                            Task {
+//                                do {
+//                                    // write code to process user change of location
+//                                    
+//                                    try await weatherMapViewModel.getCoordinatesForCity(cityName: "\(temporaryCity)")
+//                                    
+//    //                                print("NEW LOCATION")
+//    //                                print("\(newLocation)")
+//    //                                print("\(weatherMapViewModel.coordinates?.latitude)")
+//    //                                print("\(weatherMapViewModel.coordinates?.longitude)")
+//                                    
+//                                    _ = try await weatherMapViewModel.loadData(lat: weatherMapViewModel.coordinates?.latitude ?? Constants.defualtLatitude, lon: weatherMapViewModel.coordinates?.longitude ?? Constants.defualtLongitude)
+//                                    
+//    //                                print("========================================><===========")
+//                                    
+//                                    
+//                                    
+//                                } catch {
+//                                    print("Error: \(error)")
+//                                    isLoading = false
+//                                }
+//                            }
+//                        }
                 }
                 .bold()
                 .font(.system(size: 20))
@@ -67,57 +145,93 @@ struct WeatherNowView: View {
                 .font(.custom("Arial", size: 26))
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .cornerRadius(15)
-                let timestamp = TimeInterval(weatherMapViewModel.weatherDataModel?.current.dt ?? 0)
-                let formattedDate = DateFormatterUtils.formattedDateTime(from: timestamp)
-                Text(formattedDate)
-                    .padding()
-                    .font(.title)
-                    .foregroundColor(.black)
-                    .shadow(color: .black, radius: 1)
-
-                VStack{
-                   
-                    // Weather Temperature Value
-                    if let forecast = weatherMapViewModel.weatherDataModel {
-                        
-                        let des =  String(forecast.current.weather[0].weatherDescription.rawValue)
-                        
-//                        Image(forecast.current.weather[0].icon)
-                        
-                        Label {
-                            Text(des)
-                        } icon: {
-                           AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(forecast.current.weather[0].icon)@2x.png"))
-                        }
-
-
-                        
-                        Text("Temp: \((Double)(forecast.current.temp), specifier: "%.2f") ºC")
-                            .font(.system(size: 25, weight: .medium))
-                        
-                        Text("Humidity: \((Double)(forecast.current.humidity), specifier: "%.2f") %")
-                            .font(.system(size: 25, weight: .medium))
-                        
-                        Text("Pressure: \((Double)(forecast.current.pressure), specifier: "%.2f") hPa")
-                            .font(.system(size: 25, weight: .medium))
-                        
-                        Text("Wind Speed: \((Double)(forecast.current.windSpeed), specifier: "%.2f") mph")
-                            .font(.system(size: 25, weight: .medium))
-                    } else {
-                        Text("Temp: N/A")
-                            .font(.system(size: 25, weight: .medium))
-                    }
-                    
-                }
-               
-            }//VS2
-        }// VS1
+//                VStack{
+//                    HStack{
+////                        Text("Current Location: \(weatherMapViewModel.city)")
+//                    }
+//                    .bold()
+//                    .font(.system(size: 20))
+//                    .padding(10)
+//                    .shadow(color: .blue, radius: 10)
+//                    .cornerRadius(10)
+//                    .fixedSize()
+//                    .font(.custom("Arial", size: 26))
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//                    .cornerRadius(15)
+//                    let timestamp = TimeInterval(weatherMapViewModel.weatherDataModel?.current.dt ?? 0)
+//                    let formattedDate = DateFormatterUtils.formattedDateTime(from: timestamp)
+//                    Text(formattedDate)
+//                        .padding()
+//                        .font(.title)
+//                        .foregroundColor(.black)
+//                        .shadow(color: .black, radius: 1)
+//
+//                    VStack{
+//                       
+//                        // Weather Temperature Value
+//                        if let forecast = weatherMapViewModel.weatherDataModel {
+//                            
+//                            let des =  String(forecast.current.weather[0].weatherDescription.rawValue)
+//                            
+//    //                        Image(forecast.current.weather[0].icon)
+//                            
+//                            Label {
+//                                Text(des)
+//                            } icon: {
+//                               AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(forecast.current.weather[0].icon)@2x.png"))
+//                            }
+//
+//
+//                            
+//                            Text("Temp: \((Double)(forecast.current.temp), specifier: "%.2f") ºC")
+//                                .font(.system(size: 25, weight: .medium))
+//                                .foregroundColor(.white)
+//                            
+//                            Text("Humidity: \((Double)(forecast.current.humidity), specifier: "%.2f") %")
+//                                .font(.system(size: 25, weight: .medium))
+//                                .foregroundColor(.white)
+//                            
+//                            Text("Pressure: \((Double)(forecast.current.pressure), specifier: "%.2f") hPa")
+//                                .font(.system(size: 25, weight: .medium))
+//                                .foregroundColor(.white)
+//                            
+//                            Text("Wind Speed: \((Double)(forecast.current.windSpeed), specifier: "%.2f") mph")
+//                                .font(.system(size: 25, weight: .medium))
+//                                .foregroundColor(.white)
+//                        } else {
+//                            Text("Temp: N/A")
+//                                .font(.system(size: 25, weight: .medium))
+//                                .foregroundColor(.white)
+//                        }
+//                        
+//                    }
+//                    .background(Blur(style: .systemMaterial).opacity(5))
+//                   
+//                }//VS2
+            }// VS1
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
 }
 struct WeatherNowView_Previews: PreviewProvider {
     static var previews: some View {
         WeatherNowView()
     }
+}
+
+
+struct Blur: UIViewRepresentable {
+    let style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let blurEffect = UIBlurEffect(style: style)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        return visualEffectView
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {}
 }
 
 
